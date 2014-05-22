b4e6c2527ff160eac9cd8668e28c626fb044e699
Jeff Hinrichs <jeffh@dundeemt.com>
2014-05-22 17:50:38 -0500

    refactored test for new public test server, removed unneeded code

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -5,40 +5,23 @@ import sys, os
 MYPATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, MYPATH + '/../')
 
-from io import BytesIO
-import pytest
 import pysftp
 
-# pylint: disable=E1101
 # pylint: disable = W0142
-SFTP_PUBLIC = {'host':'test.rebex.net', 'username':'demo',
-               'password':'password'}
-SFTP_LOCAL = {'host':'localhost', 'username':'test', 'password':'test1357'}
- #can only reach public, read-only server from CI platform, only test locally
- # if environment variable CI is set  to something to disable local tests
- # the CI env var is set to true by both drone-io and travis
-skip_if_ci = pytest.mark.skipif(os.getenv('CI', '')>'', reason='Not Local')
+SFTP_PUBLIC = {'host':'68.226.78.92', 'username':'test',
+               'password':'test1357', 'port':2222}
 
 def test_normalize():
     '''test the normalize function'''
     with pysftp.Connection(**SFTP_PUBLIC) as sftp:
-        assert sftp.normalize('readme.txt') == '/readme.txt'
-        assert sftp.normalize('.') == '/'
-        assert sftp.normalize('pub') == '/pub'
+        assert sftp.normalize('readme.txt') == '/home/test/readme.txt'
+        assert sftp.normalize('.') == '/home/test'
+        assert sftp.normalize('pub') == '/home/test/pub'
         sftp.chdir('pub')
-        assert sftp.normalize('.') == '/pub'
+        assert sftp.normalize('.') == '/home/test/pub'
 
-@skip_if_ci
 def test_normalize_symlink():
     '''test normalize against a symlink'''
-    flo = BytesIO('*'*8192)
-    rfile = 'read-me.txt'
-    rsym = 'read-me.sym'
-    with pysftp.Connection(**SFTP_LOCAL) as sftp:
-        sftp.putfo(flo, rfile)
-        sftp.symlink(rfile, rsym)
-        rslvs_correct = sftp.normalize(rsym) == '/home/test/read-me.txt'
-        # clean up
-        sftp.remove(rfile)
-        sftp.remove(rsym)
-    assert rslvs_correct
+    rsym = 'readme.sym'
+    with pysftp.Connection(**SFTP_PUBLIC) as sftp:
+        assert sftp.normalize(rsym) == '/home/test/readme.txt'
