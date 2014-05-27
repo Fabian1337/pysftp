aa889458c5d11162c22b8f729365ffe4f5db6206
Jeff Hinrichs <jeffh@dundeemt.com>
2014-05-26 21:54:00 -0500

    stripped out and updated chmod tests. See issue #48

---
+++ tests/test_chmod.py
@@ -0,0 +1,46 @@
+'''test pysftp.Connection.chmod - uses py.test'''
+from __future__ import print_function
+
+# pylint: disable = W0142
+from common import *
+import pytest
+
+def test_chmod_not_exist():
+    '''verify error if trying to chmod something that isn't there'''
+    with pytest.raises(IOError):
+        with pysftp.Connection(**SFTP_PUBLIC) as sftp:
+            sftp.chmod('i-do-not-exist.txt', 666)
+
+@skip_if_ci
+def test_chmod_simple():
+    '''test basic chmod with octal mode represented by an int`'''
+    new_mode = 744      #user=rwx g=r o=r
+    with tempfile_containing('') as fname:
+        base_fname = os.path.split(fname)[1]
+        with pysftp.Connection(**SFTP_LOCAL) as sftp:
+            org_attrs = sftp.put(fname)
+            sftp.chmod(base_fname, new_mode)
+            new_attrs = sftp.stat(base_fname)
+            sftp.remove(base_fname)
+    # that the new mod 711 is as we wanted
+    assert pysftp.st_mode_to_int(new_attrs.st_mode) == new_mode
+    # that we actually changed something
+    assert new_attrs.st_mode != org_attrs.st_mode
+
+def test_chmod_fail_ro():
+    '''test chmod against read-only server'''
+    new_mode = 740
+    fname = 'readme.txt'
+    with pysftp.Connection(**SFTP_PUBLIC) as sftp:
+        org_attrs = sftp.stat(fname)
+        sftp.chmod(fname, new_mode)
+        new_attrs = sftp.stat(fname)
+        #reset the mode back
+        sftp.chmod(fname, pysftp.st_mode_to_int(org_attrs.st_mode))
+
+    # that the new mod 711 is as we wanted
+    assert pysftp.st_mode_to_int(new_attrs.st_mode) == new_mode
+    # that we actually changed something
+    assert new_attrs.st_mode != org_attrs.st_mode
+    assert pysftp.st_mode_to_int(org_attrs.st_mode) == 744
+
--- tests/test_pysftp_02.py
+++ tests/test_pysftp_02.py
@@ -67,29 +67,6 @@ def test_chown_ro_server():
         with pysftp.Connection(**SFTP_PUBLIC) as sftp:
             sftp.chown('readme.txt', gid=1000, uid=1000)
 
-@skip_if_ci
-def test_chmod_not_exist():
-    '''verify error if trying to chmod something that isn't there'''
-    with pytest.raises(IOError):
-        with pysftp.Connection(**SFTP_LOCAL) as sftp:
-            sftp.chmod('i-do-not-exist.txt', 666)
-
-@skip_if_ci
-def test_chmod_simple():
-    '''test basic chmod'''
-    new_mode = 711
-    with tempfile_containing('') as fname:
-        base_fname = os.path.split(fname)[1]
-        with pysftp.Connection(**SFTP_LOCAL) as sftp:
-            org_attrs = sftp.put(fname)
-            sftp.chmod(base_fname, new_mode)
-            new_attrs = sftp.stat(base_fname)
-            sftp.remove(base_fname)
-    # that the new mod 711 is as we wanted
-    assert pysftp.st_mode_to_int(new_attrs.st_mode) == new_mode
-    # that we actually changed something
-    assert new_attrs.st_mode != org_attrs.st_mode
-
 def test_path_retreat():
     '''test path_retreat generator'''
     pth = 'foo/bar/baz'
