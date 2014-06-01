aa42b9c8bc5511077b56b961bb4391757ab16f3f
Jeff Hinrichs <jeffh@dundeemt.com>
2014-06-01 07:33:49 -0500

    allowance for new testing server location

--- tests/common.py
+++ tests/common.py
@@ -12,8 +12,14 @@ from dhp.test import tempfile_containing
 
 
 # pylint: disable=E1101
-SFTP_PUBLIC = {'host':'68.226.78.92', 'username':'test',
-               'password':'test1357', 'port':2222}
+if os.path.exists('/home/jlh/projects/pysftp'):
+    SFTP_PUBLIC = {'host':'srv01.dundeemt.pri', 'username':'test',
+                   'password':'test1357'}
+else:
+    SFTP_PUBLIC = {'host':'174.74.39.157', 'username':'test',
+                   'password':'test1357', 'port':2222}
+# SFTP_PUBLIC = {'host':'test.rebex.net', 'username':'demo',
+#                'password':'password'}
 SFTP_LOCAL = {'host':'localhost', 'username':'test', 'password':'test1357'}
  #can only reach public, read-only server from CI platform, only test locally
  # if environment variable CI is set  to something to disable local tests
