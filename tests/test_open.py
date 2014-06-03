d6daa32b175df66118d5f99f66ab52729dcf0104
Jeff Hinrichs <jeffh@dundeemt.com>
2014-06-03 17:40:32 -0500

    converted to use fixtures

--- tests/test_open.py
+++ tests/test_open.py
@@ -3,17 +3,17 @@
 # pylint: disable = W0142
 from common import *
 
-def test_open_read():
+def test_open_read(psftp):
     '''test the open function'''
-    with pysftp.Connection(**SFTP_PUBLIC) as sftp:
-        rfile = sftp.open('readme.txt')
-        contents = rfile.read()
-        rfile.close()
+    psftp.chdir('/home/test')
+    rfile = psftp.open('readme.txt')
+    contents = rfile.read()
+    rfile.close()
     assert contents[0:9] == b'This SFTP'
 
-def test_open_read_with():
+def test_open_read_with(psftp):
     '''test the open function in a with statment'''
-    with pysftp.Connection(**SFTP_PUBLIC) as sftp:
-        with sftp.open('readme.txt') as rfile:
-            contents = rfile.read()
+    psftp.chdir('/home/test')
+    with psftp.open('readme.txt') as rfile:
+        contents = rfile.read()
     assert contents[0:9] == b'This SFTP'
