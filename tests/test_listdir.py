2ef0d3a1b324343ea780af331691587924f8d97c
Jeff Hinrichs <jeffh@dundeemt.com>
2014-06-03 08:26:58 -0500

    converted to use fixtures

--- tests/test_listdir.py
+++ tests/test_listdir.py
@@ -3,24 +3,23 @@
 # pylint: disable = W0142
 from common import *
 
-def test_listdir():
+def test_listdir(psftp):
     '''test listdir'''
-    sftp = pysftp.Connection(**SFTP_PUBLIC)
-    assert sftp.listdir() == ['pub', 'readme.sym', 'readme.txt']
-    sftp.close()
+    psftp.cwd('/home/test')
+    assert psftp.listdir() == ['pub', 'readme.sym', 'readme.txt']
 
 
-def test_listdir_attr():
+def test_listdir_attr(psftp):
     '''test listdir'''
-    with pysftp.Connection(**SFTP_PUBLIC) as sftp:
-        attrs = sftp.listdir_attr()
-        assert len(attrs) == 3
-        # test they are in filename order
-        assert attrs[0].filename == 'pub'
-        assert attrs[1].filename == 'readme.sym'
-        assert attrs[2].filename == 'readme.txt'
-        # test that longname is there
-        for attr in attrs:
-            assert attr.longname is not None
+    psftp.cwd('/home/test')
+    attrs = psftp.listdir_attr()
+    assert len(attrs) == 3
+    # test they are in filename order
+    assert attrs[0].filename == 'pub'
+    assert attrs[1].filename == 'readme.sym'
+    assert attrs[2].filename == 'readme.txt'
+    # test that longname is there
+    for attr in attrs:
+        assert attr.longname is not None
 
 
