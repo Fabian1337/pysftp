13644c695784198934255cf50ddaee8eac7d07d1
Jeff Hinrichs <jeffh@dundeemt.com>
2014-06-03 00:21:27 -0500

    converted over to fixtures to speed up the tests

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -5,24 +5,21 @@ from common import *
 from dhp.VI import py_ver
 
 
-def test_execute_simple_ro():
+def test_execute_simple_ro(psftp):
     '''test execute simple on a read-only server '''
-    with pysftp.Connection(**SFTP_PUBLIC) as sftp:
-        # always starts at no timeout,
-        results = [b'This service allows sftp connections only.\n',]
-        assert sftp.execute('ls') == results
+    results = [b'This service allows sftp connections only.\n',]
+    assert psftp.execute('ls') == results
 
 @skip_if_ci
-def test_execute_simple():
+def test_execute_simple(lsftp):
     '''test execute simple'''
     if py_ver() == 2:
         type_check = basestring
     else:
         type_check = str
 
-    with pysftp.Connection(**SFTP_LOCAL) as sftp:
-        # always starts at no timeout,
-        results = sftp.execute('ls')
-        for result in results:
-            assert isinstance(result, type_check)
+    results = lsftp.execute('ls')
+    # confirm results are an iterable of strings (version dependent)
+    for result in results:
+        assert isinstance(result, type_check)
 
