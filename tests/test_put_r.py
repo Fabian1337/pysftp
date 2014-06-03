10072a2aff13c8a145572561db63e64b03c35897
Jeff Hinrichs <jeffh@dundeemt.com>
2014-06-03 18:58:11 -0500

    converted over to fixtures to speed up the tests

--- tests/test_put_r.py
+++ tests/test_put_r.py
@@ -11,7 +11,7 @@ import shutil
 
 
 @skip_if_ci
-def test_put_r():
+def test_put_r(lsftp):
     '''test put_r'''
     localpath = mkdtemp()
     print(localpath)
@@ -19,29 +19,28 @@ def test_put_r():
     build_dir_struct(localpath)
     localpath = os.path.join(localpath, 'pub')
     print(localpath)
-    with pysftp.Connection(**SFTP_LOCAL) as sftp:
-        # make a tidy place to put them
-        sftp.mkdir(remote_dir)
-        # run the op
-        sftp.put_r(localpath, remote_dir)
+    # make a tidy place to put them
+    lsftp.mkdir(remote_dir)
+    # run the op
+    lsftp.put_r(localpath, remote_dir)
 
-        # inspect results
-        rfs = pysftp.WTCallbacks()
-        with sftp.cd(remote_dir):
-            sftp.walktree('.', rfs.file_cb, rfs.dir_cb, rfs.unk_cb)
+    # inspect results
+    rfs = pysftp.WTCallbacks()
+    with lsftp.cd(remote_dir):
+        lsftp.walktree('.', rfs.file_cb, rfs.dir_cb, rfs.unk_cb)
 
-        lfs = pysftp.WTCallbacks()
-        save_dir = os.getcwd()
-        os.chdir(localpath)
-        walktree('.', lfs.file_cb, lfs.dir_cb, lfs.unk_cb)
-        os.chdir(save_dir)
+    lfs = pysftp.WTCallbacks()
+    save_dir = os.getcwd()
+    os.chdir(localpath)
+    walktree('.', lfs.file_cb, lfs.dir_cb, lfs.unk_cb)
+    os.chdir(save_dir)
 
-        # cleanup remote
-        for fname in rfs.flist:
-            sftp.remove(reparent(remote_dir, fname))
-        for dname in reversed(rfs.dlist):
-            sftp.rmdir(reparent(remote_dir, dname))
-        sftp.rmdir(remote_dir)
+    # cleanup remote
+    for fname in rfs.flist:
+        lsftp.remove(reparent(remote_dir, fname))
+    for dname in reversed(rfs.dlist):
+        lsftp.rmdir(reparent(remote_dir, dname))
+    lsftp.rmdir(remote_dir)
 
     # cleanup local
     shutil.rmtree(os.path.split(localpath)[0])
@@ -61,16 +60,14 @@ def test_put_r():
     assert rfs.ulist == lfs.ulist
     assert rfs.ulist == []
 
-def test_put_r_ro():
+def test_put_r_ro(psftp):
     '''test put_r failure on remote read-only srvr'''
-    with pysftp.Connection(**SFTP_PUBLIC) as sftp:
-        # run the op
-        with pytest.raises(IOError):
-            sftp.put_r('.', '.')
+    # run the op
+    with pytest.raises(IOError):
+        psftp.put_r('.', '.')
 
-def test_put_r_bad_local():
+def test_put_r_bad_local(psftp):
     '''test put_r failure on non-existing local directory'''
-    with pysftp.Connection(**SFTP_PUBLIC) as sftp:
-        # run the op
-        with pytest.raises(OSError):
-            sftp.put_r('/non-existing', '.')
+    # run the op
+    with pytest.raises(OSError):
+        psftp.put_r('/non-existing', '.')
