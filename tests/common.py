faf9dbdbd5d5bdc5b24c37eaedbc1f4878ddd896
Jeff Hinrichs <dundeemt@gmail.com>
2015-04-03 23:57:54 -0500

    add inline markers to quiet pep8 about importing after adjusting the sys.path. Closes issue #75

--- tests/common.py
+++ tests/common.py
@@ -5,12 +5,12 @@ import sys
 import os
 MYPATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, MYPATH + '/../')
-import warnings
+import warnings     # noqa
 
-import pysftp
+import pysftp       # noqa
 
-import pytest
-from dhp.test import tempfile_containing
+import pytest       # noqa
+from dhp.test import tempfile_containing    # noqa
 
 
 # pylint: disable=E1101
@@ -23,6 +23,8 @@ else:
 # SFTP_PUBLIC = {'host':'test.rebex.net', 'username':'demo',
 #                'password':'password'}
 SFTP_LOCAL = {'host': 'localhost', 'username': 'test', 'password': 'test1357'}
+
+SFTP_INTERNAL = {'host': 'localhost', 'username': 'user', 'password': 'pw'}
 # can only reach public, read-only server from CI platform, only test locally
 # if environment variable CI is set  to something to disable local tests
 # the CI env var is set to true by both drone-io and travis
@@ -35,3 +37,25 @@ def warnings_as_errors(request):
     warnings.simplefilter('error')
 
     request.addfinalizer(lambda *args: warnings.resetwarnings())
+
+
+def conn(sftpsrv):
+    """return a dictionary holding connection info for the pysftp client"""
+    return {'host': sftpsrv.host, 'port': sftpsrv.port, 'username': 'user',
+            'password': 'pw'}
+
+CONTENT = {'pub': {
+                        'make.txt': "content of make.txt",
+                        'foo1': {
+                                'foo1.txt': 'content of foo1.txt',
+                                'image01.jpg': 'data for image01.jpg'
+                        },
+                        'foo2': {
+                                'foo2.txt': 'content of foo2.txt',
+                                'bar1': {
+                                    'bar1.txt': 'contents bar1.txt'
+                                }
+                        }
+                    },
+           'read.me': 'contents of read.me',
+           }
