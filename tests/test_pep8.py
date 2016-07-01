fe070007a2c4197fcc91f7799b066728a986d76e
Jeff Hinrichs <dundeemt@gmail.com>
2016-07-01 09:36:29 -0500

    minor structural change, use a proper package instead of a simple file for pysftp

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -5,7 +5,7 @@ import pep8
 def test_pep8():
     '''pep8 check the source'''
     # list the specific files or directories to check, directories are recursed
-    paths = ['pysftp.py', 'setup.py', 'tests']
+    paths = ['pysftp', 'setup.py', 'tests']
     p8c = pep8.StyleGuide()
     report = p8c.check_files(paths=paths)
     report.print_statistics()
